b2424a3203315606a83aa1b4b4a98d46e76759bf
iqbal2508 <rasyidiqbal52@gmail.com>
2025-06-26 19:46:34 +0700

    Pesan perubahan

--- src/pages/api/kursus/index.ts
+++ src/pages/api/kursus/index.ts
@@ -1,55 +1,35 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import clientPromise from '../../../lib/mongodb';
+import { Content } from 'next/font/google';
+import { title } from 'process';
 
 // Definisikan tipe data untuk 1 item kursus
-type Kursus = {
-  _id: string;
-  nama: string;
-  deskripsi: string;
-  // Tambahkan field lain jika ada, seperti:
-  // harga: number;
-  // durasi: string;
-};
 
 // Tipe untuk response API
-type ResponseData = {
-  message: string;
-  status: number;
-  data: Kursus[];
-};
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<ResponseData>
+  res: NextApiResponse
 ) {
   const client = await clientPromise;
   const db = client.db("elearningIqbal");
 
   switch (req.method) {
     case "GET":
-      try {
-        const allPosts = await db.collection("kursus").find({}).toArray();
-        res.status(200).json({
-          status: 200,
-          data: allPosts as Kursus[],
-          message: 'Data berhasil diambil'
-        });
-      } catch (error) {
-        console.error("Error mengambil data kursus:", error); // fix untuk ESLint
-        res.status(500).json({
-          status: 500,
-          data: [],
-          message: 'Terjadi kesalahan saat mengambil data'
-        });
+      const filter = req.query.filter || '';
+      const options = {
+        projection: {
+          content: 0
+        }
       }
-      break;
-
-    default:
-      res.status(405).json({
-        status: 405,
-        data: [],
-        message: 'Metode tidak diizinkan'
+      const kursus = await db.collection("kursus").find({
+        title:{ $regex: filter, $option: 'i'},
+      },options).toArray();
+      res.json({
+        status: 200, data: kursus,
+        message: ''
       });
+      break;
+    }
   }
-}
-//APAAAA
+      
